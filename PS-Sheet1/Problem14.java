4b88f76c4d1ec2ed72901a3164ee4e089f8bf9db
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-23 20:51:30 +0530

    updates

---
+++ PS-Sheet1/Problem14.java
@@ -0,0 +1,19 @@
+/*
+ * Finding the Largest and Smallest Numbers in an Array
+Difficulty: Easy
+Topics: Basic Programming, Arrays
+Description: Write a program to find the largest and smallest numbers in an array.
+Example:
+Input: array = [4, 7, 1, 8, 5]
+Output: Largest: 8, Smallest: 1
+Explanation: The largest number in the array is 8 and the smallest is 1.
+ */
+
+import java.util.Scanner;
+
+public class Problem14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
