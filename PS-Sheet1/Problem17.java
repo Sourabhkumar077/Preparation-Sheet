c2e02eab51b2965fed7d565d212225cfdbeb3980
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-25 12:40:11 +0530

    Updates

--- PS-Sheet1/Problem17.java
+++ PS-Sheet1/Problem17.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Checking for Armstrong Numbers in a Range
 Difficulty: Easy
@@ -10,6 +12,39 @@ Explanation: Armstrong numbers between 1 and 500 are 1, 153, 370, 371, and 407.
  */
 public class Problem17 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the start of range: ");
+        int start = sc.nextInt();
+        System.out.print("enter the ending of range : ");
+        int end = sc.nextInt();
+        armstrongInRange(start,end);
+
+    }
+    public static void armstrongInRange(int start , int end){
+        for(int i = start; i <= end; i++){
+            if(checkArmstrong(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean checkArmstrong(int number){
+        int temp = number;
+//        find length of number
+        int numLength = 0;
+        while(temp!= 0) {
+            temp /= 10;
+            numLength++;
+        }
+         temp = number;
+//        check for armstrong
+        int result = 0;
+        while(temp != 0){
+            int unitDigit = temp % 10;
+            result += (int)Math.pow(unitDigit,numLength);
+            temp /= 10;
+        }
+
+        return result == number;
 
     }
 }
