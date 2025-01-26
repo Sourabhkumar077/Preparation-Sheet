4b8852f6d078994fb66f48542087b3700f3d4fbf
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-26 15:44:23 +0530

    updates here

--- PS-Sheet1/Problem24.java
+++ PS-Sheet1/Problem24.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Printing Prime Numbers Less Than a Given Number
 Difficulty: Easy
@@ -9,4 +11,28 @@ Output: 2, 3, 5, 7, 11, 13, 17, 19
 Explanation: The prime numbers less than 20 are 2, 3, 5, 7, 11, 13, 17, and 19.
  */
 public class Problem24 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int number = s.nextInt();
+        s.close();
+//        final function call
+        primesLessThanRange(number);
+
+    }
+    public static void primesLessThanRange(int number){
+        for (int i = 2; i <= number; i++){
+            if(checkPrime(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean checkPrime(int num){
+        for(int i = 2; i <= num /2 ; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
