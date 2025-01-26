40b069631e8f6325a0f61929943b6418181b63f7
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-26 15:06:23 +0530

    Updates

--- PS-Sheet1/Problem21.java
+++ PS-Sheet1/Problem21.java
@@ -9,8 +9,27 @@ Output: 30
 Explanation: The sum of even numbers between 1 and 10 is 2 + 4 + 6 + 8 + 10 = 30.
  */
 
+import java.util.Scanner;
+
 public class Problem21 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Start number : ");
+        int start = sc.nextInt();
+        System.out.print("Enter End number : ");
+        int end = sc.nextInt();
+        sc.close();
 
+        System.out.println("Sum of even number between " + start + " and "+ end +" is "+ sumOfEvensInRange(start,end));
+
+    }
+    public static int sumOfEvensInRange(int start , int end){
+        int sum = 0;
+        for(int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
--- PS-Sheet1/Problem22.java
+++ PS-Sheet1/Problem22.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Calculating the Sum of Odd Numbers in a Range
 Difficulty: Easy
@@ -9,4 +11,24 @@ Output: 25
 Explanation: The sum of odd numbers between 1 and 10 is 1 + 3 + 5 + 7 + 9 = 25.
  */
 public class Problem22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Start number : ");
+        int start = sc.nextInt();
+        System.out.print("Enter End number : ");
+        int end = sc.nextInt();
+        sc.close();
+
+        System.out.println("Sum of odd number between " + start + " and "+ end +" is "+ sumOfOddInRange(start,end));
+
+    }
+    public static int sumOfOddInRange(int start,int end){
+        int oddSum = 0;
+        for(int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                oddSum += i;
+            }
+        }
+        return oddSum;
+    }
 }
