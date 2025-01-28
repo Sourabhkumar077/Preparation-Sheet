e8d6d17a72fcac9a89ffa844a4a6835214308d28
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-28 12:37:45 +0530

    updates

--- PS-Sheet1/Problem30.java
+++ PS-Sheet1/Problem30.java
@@ -9,8 +9,27 @@ Output: [3]
 Explanation: The missing number in the sequence from 1 to 5 is 3.
  */
 
+import java.util.Arrays;
+
 public class Problem30 {
     public static void main(String[] args) {
-        
+        int arr[] = { 1, 2,3, 4, 5, 6 ,5,7,9,12,13,24, };
+        Arrays.sort(arr);
+        System.out.println("Sorted Array : " + Arrays.toString(arr));
+        int ele = 1;
+        for (int i = 0; i < arr.length; i++) {
+            // Skip duplicate elements
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            if (arr[i] == ele) {
+                ele++;
+            } else if (arr[i] > ele) {
+                System.out.println("Missing Number is : " + ele);
+                return; // Exit as soon as the first missing number is found
+            }
+        }
+
+        System.out.println("Missing Number is : " + ele);
     }
 }
