4e1e79d2b06de0e1d9f570963ec8e958dfe97679
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-29 11:35:52 +0530

    updates

--- PS-Sheet1/Problem32.java
+++ PS-Sheet1/Problem32.java
@@ -8,9 +8,20 @@ Input: array = [3, 1, 2, 4, 5]
 Output: 3
 Explanation: The median of the sorted array [1, 2, 3, 4, 5] is 3.
  */
+
 public class Problem32 {
-    public static void main(String[] args) {
-        
+   public static void main(String[] args) {
+    int arr[]  = {1,2,3,4,5,};
+    int median = findMedian(arr);
+    System.out.println("Median of the array is : " + median);
     }
-    
+    public static int findMedian(int[] arr) {
+        int n = arr.length;
+        int mid = n / 2;
+        if (n % 2 == 0) {
+            return (arr[mid - 1] + arr[mid]) / 2;
+        } else {
+            return arr[mid];
+        }
+    }
 }
--- PS-Sheet1/Problem34.java
+++ PS-Sheet1/Problem34.java
@@ -8,8 +8,41 @@ Input: string1 = "listen", string2 = "silent"
 Output: True
 Explanation: "listen" and "silent" are anagrams of each other
  */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem34 {
-    public static void main(String[] args) {
-        
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string : ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second string : ");
+        String str2 = sc.nextLine();
+        sc.close();
+
+        if (isAnagram(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+    public static boolean isAnagram(String str1, String str2) {
+        // replace all white space and convert to lowercase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // If lengths are not equal, they can't be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert to char array and sort them
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
     }
 }
