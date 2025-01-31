1e23d9daab773db380f0719d5969eec5f390515a
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-01-31 16:16:52 +0530

    upadates

--- PS-Sheet1/Problem41.java
+++ PS-Sheet1/Problem41.java
@@ -8,8 +8,30 @@ Input: number = 122333, digit = 3
 Output: 3
 Explanation: The digit 3 occurs 3 times in the number 122333.
  */
+
+import java.util.Scanner;
+
 public class Problem41 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        Integer number = sc.nextInt();
+        System.out.print("Enter a digit: ");
+        Integer digit = sc.nextInt();
+        sc.close();
+        countOfOcuurences(number, digit);
+    }
+
+    public static void countOfOcuurences(Integer number, Integer digit) {
+        int count = 0;
+        char digitChar = Character.forDigit(digit, 10);
+        String num = number.toString();
+        for (int i = 0; i < num.length(); i++) {
+            if (digitChar == num.charAt(i)) {
+                count++;
+            }
+        }
+         System.out.println("The digit " + digitChar + " occurs " + count + " times in the number " + number);
+
     }
 }
