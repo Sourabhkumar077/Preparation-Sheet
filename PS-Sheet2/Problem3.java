cad40e73ddd3536e2cb0cf7d6630df9a8a171e52
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-04 10:28:26 +0530

    updates

--- PS-Sheet2/Problem3.java
+++ PS-Sheet2/Problem3.java
@@ -8,9 +8,25 @@ Input: n = 4
 Output: 2.083333
 Explanation: Sum of the series is 1 + 1/2 + 1/3 + 1/4 ≈ 2.083333.
  */
+
+import java.util.Scanner;
+
 public class Problem3 {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        double sum = calculateSum(n);
+        System.out.println("The sum of the series is: " + sum);
+        sc.close();
+    }
+
+    public static double calculateSum(int n) {
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        return sum;
     }
 }
