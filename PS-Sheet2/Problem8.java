67e4245b753f9b5941491e1413cc831269f3b661
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-13 19:55:44 +0530

    new Updates

--- PS-Sheet2/Problem8.java
+++ PS-Sheet2/Problem8.java
@@ -9,32 +9,29 @@ Input: array = [12, 24, 36]
 Output: 12
 Explanation: The GCD of 12, 24, and 36 is 12.
  */
+
 public class Problem8 {
     public static void main(String[] args) {
-      // int arr[] = {12, 24, 36}; 
-      findGcdOfNum(12);
-    }
-    public static void findGcd(int[] arr){
-      int max = Integer.MIN_VALUE;
-        for(int i = 0; i < arr.length; i++){
-          if(arr[i ] > max){
-            max = arr[i];
-          }
-        }
-
-        for(int i = max; i > 0 || arr.length > 0; i--){
-          
-        }
+        int arr[] = {12, 24, 36}; 
+        System.out.println("GCD of array: " + findGcd(arr));
     }
 
-    public static void findGcdOfNum(int num){
-      int gcd = num;
-      for(int i = num - 1; i > 0; i--){
-        if( num % i == 0){
-          gcd = i;
-          break;
+    // Function to find GCD of two numbers using Euclidean Algorithm
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-      }
-      System.out.println(gcd);
+        return a;
+    }
+
+    // Function to find GCD of an array
+    public static int findGcd(int[] arr) {
+        int result = arr[0]; // Start with first element
+        for (int i = 1; i < arr.length; i++) {
+            result = gcd(result, arr[i]);
+        }
+        return result;
     }
 }
