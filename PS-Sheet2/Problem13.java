83a82c01763b8a59ed6a786079bbbd3b9d7b3911
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-14 23:59:32 +0530

    updates

--- PS-Sheet2/Problem13.java
+++ PS-Sheet2/Problem13.java
@@ -12,4 +12,13 @@ public class Problem13 {
     public static void main(String[] args) {
         
     }
+
+    public static boolean heckPrime(int num){
+        for( int i  = 2; i < num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
