bdc7ebbe4078b933b680f6e1790b17952a011a00
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-15 09:55:58 +0530

    Updates

--- PS-Sheet2/Problem13.java
+++ PS-Sheet2/Problem13.java
@@ -8,12 +8,30 @@ Input: N = 4
 Output: 17
 Explanation: The sum of the first 4 prime numbers (2 + 3 + 5 + 7) is 17.
  */
+
+import java.util.Scanner;
+
 public class Problem13 {
     public static void main(String[] args) {
-        
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Nth Number : ");
+        int range = sc.nextInt();
+        System.out.println("Prime Number sum efore " + range + " is  = " + primeNumSum(range));
+        sc.close();
 
-    public static boolean heckPrime(int num){
+    }
+    public static int primeNumSum(int range){
+        int count = 0;
+        int primeSum = 0;
+        for(int i = 2; count < range; i++){
+            if(checkPrime(i)){
+                primeSum += i;
+                count++;
+            }
+        }
+        return primeSum;
+    }
+    public static boolean checkPrime(int num){
         for( int i  = 2; i < num; i++){
             if(num % i == 0){
                 return false;
