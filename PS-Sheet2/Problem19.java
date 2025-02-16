b4f920f0fd8c1aa02ae517114a04cd75b228541f
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-16 12:03:56 +0530

    Updates

--- PS-Sheet2/Problem19.java
+++ PS-Sheet2/Problem19.java
@@ -10,8 +10,28 @@ Input: start = 1, end = 10
 Output: [1, 4, 9]
 Explanation: Perfect squares between 1 and 10 are 1, 4, and 9.
  */
+
+import java.util.Scanner;
+
 public class Problem19 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter start of range : ");
+        int start = sc.nextInt();
+        System.out.print("Enter end of range : ");
+        int end = sc.nextInt();
+        perfectSquareInRange(start, end);
+        sc.close();
+    }
+    public static void perfectSquareInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPerfectSquare(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    static boolean isPerfectSquare(int n) {
+        int sqrt = (int) Math.sqrt(n);
+        return sqrt * sqrt == n;
     }
 }
--- PS-Sheet2/Problem20.java
+++ PS-Sheet2/Problem20.java
@@ -4,12 +4,23 @@ Difficulty: Easy
 Topics: Matrix Operations
 Description: Write a program to calculate the sum of the diagonal elements in a square matrix.
 Example:
-Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Input: matrix = [[1, 2, 3], 
+                [4, 5, 6], 
+                [7, 8, 9]]
 Output: 15
 Explanation: The sum of the diagonal elements (1 + 5 + 9) is 15.
  */
 public class Problem20 {
     public static void main(String[] args) {
-        
+        int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(sumOfDiagonalElements(matrix));
+    }
+
+    public static int sumOfDiagonalElements(int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+        }
+        return sum;
     }
 }
