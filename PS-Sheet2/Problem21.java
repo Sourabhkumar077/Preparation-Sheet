a6daf4ae37e6022a82fa3560defbb6c50086dfd0
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-24 00:04:49 +0530

    updates

--- PS-Sheet2/Problem21.java
+++ PS-Sheet2/Problem21.java
@@ -11,6 +11,21 @@ Explanation: The second smallest number in the array is 10.
 
 public class Problem21 {
     public static void main(String[] args) {
-        
+        int arr[] = {12, 13, 1, 10, 34, 1};
+        int secondSmallest = findSecondSmallest(arr);
+        System.out.println("The second smallest number in the array is: " + secondSmallest);
+    }
+    public static int findSecondSmallest(int[] arr) {
+        int smallest = arr[0];
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int num : arr) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
+            }
+        }
+        return secondSmallest;
     }
 }
--- PS-Sheet2/Problem22.java
+++ PS-Sheet2/Problem22.java
@@ -13,6 +13,20 @@ Output:
  */
 public class Problem22 {
     public static void main(String[] args) {
-        
+        printPascal(3);
+    }
+    public static void printPascal(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(findPascal(i, j) + " "); 
+            }
+            System.out.println(); 
+        }
+    }    
+    public static int findPascal(int n, int r) {
+        if (r == 0 || r == n) {
+            return 1;
+        }
+        return findPascal(n - 1, r - 1) + findPascal(n - 1, r);
     }
 }
