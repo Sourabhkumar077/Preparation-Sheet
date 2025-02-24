7e1cc0afc53bd29782f51df8f02e31c9d49686e0
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-02-24 19:47:18 +0530

    updates

--- PS-Sheet2/Problem18.java
+++ PS-Sheet2/Problem18.java
@@ -10,6 +10,21 @@ Explanation: The subsets of [1, 2] are the empty set, [1], [2], and [1, 2].
  */
 public class Problem18 {
     public static void main(String[] args) {
-        
+        int[] set = {1, 2, 3};
+        generateSubsets(set);
+    }
+    public static void generateSubsets(int[] set) {
+        int n = set.length;
+        int max = 1 << n; // 2^n
+
+        for (int i = 0; i < max; i++) {
+            System.out.print("{");
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    System.out.print(set[j] + " ");
+                }
+            }
+            System.out.println("}");
+        }
     }
 }
