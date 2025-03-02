2b8dc63eae3d6e0e8e987f4cef922ddf94a5628a
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-02 11:54:25 +0530

    updates

--- PS-Sheet2/Problem28.java
+++ PS-Sheet2/Problem28.java
@@ -14,6 +14,15 @@ Output:
  */
 public class Problem28 {
     public static void main(String[] args) {
-        
+        int size = 4;
+        printPattern(size);
+    }
+    public static void printPattern(int size){
+        for (int i = 1; i <= size; i++) {
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
