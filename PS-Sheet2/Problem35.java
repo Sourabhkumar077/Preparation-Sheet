6c6c89cc0d3ba04a80da8442b1a8c0e153e43955
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-03 22:02:01 +0530

    updates

--- PS-Sheet2/Problem35.java
+++ PS-Sheet2/Problem35.java
@@ -14,7 +14,25 @@ Output:
 1 1 1 1  
  */
 public class Problem35 {
+    public static void printDiagonalMatrix(int size) {
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int size = 4;
+        printDiagonalMatrix(size);
     }
 }
