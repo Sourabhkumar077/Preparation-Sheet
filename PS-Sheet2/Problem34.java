f3c8aea1ea5e3e1b3cbcc81181e3cd043ad137e2
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-09 23:36:39 +0530

    updates

--- PS-Sheet2/Problem34.java
+++ PS-Sheet2/Problem34.java
@@ -10,6 +10,18 @@ Explanation: There are 2 words in the string.
  */
 public class Problem34 {
     public static void main(String[] args) {
-        
+        String string = "Hello world";
+        int wordCount = countWords(string);
+        System.out.println("\nNumber of Words in given string is : "+wordCount);
+    }
+
+    public static int countWords(String string) {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        return count + 1;
     }
 }
