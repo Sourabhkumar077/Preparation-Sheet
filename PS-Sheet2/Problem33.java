a4018f2874440d4cb38d9b958f0fed0402f4f3fb
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-09 23:41:38 +0530

    updates

--- PS-Sheet2/Problem33.java
+++ PS-Sheet2/Problem33.java
@@ -8,8 +8,22 @@ Input: list = [3, 1, 4, 1, 5]
 Output: 3
 Explanation: After sorting the list to [1, 1, 3, 4, 5], the median is 3.
  */
-public class Problem33 {
+import java.util.Arrays;
+
+ public class Problem33 {
     public static void main(String[] args) {
-        
+        int[] list = {3, 1, 4, 1, 5};
+        double median = findMedian(list);
+        System.out.println("After sorting the list : "+median);
+    }
+
+    public static double findMedian(int[] list) {
+        Arrays.sort(list);
+        int n = list.length;
+        if (n % 2 == 0) {
+            return (list[n / 2 - 1] + list[n / 2]) / 2.0;
+        } else {
+            return list[n / 2];
+        }
     }
 }
