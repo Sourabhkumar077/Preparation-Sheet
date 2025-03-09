e4b197e6ab8c0b017508596776299cde91ce4864
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-09 23:44:21 +0530

    updates

--- PS-Sheet2/Problem32.java
+++ PS-Sheet2/Problem32.java
@@ -12,6 +12,18 @@ Explanation: The substring "ab" is repeated.
  */
 public class Problem32 {
     public static void main(String[] args) {
-        
+        String string = "abab";
+        boolean repeated = hasRepeatedSubstring(string);
+        System.out.println(repeated);
+    }
+
+    public static boolean hasRepeatedSubstring(String string) {
+        for (int i = 0; i < string.length() - 1; i++) {
+            String substring = string.substring(0, i + 1);
+            if (string.indexOf(substring, i + 1) != -1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
