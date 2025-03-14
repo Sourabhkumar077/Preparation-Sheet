f6ad61ac07df83eb413d9027a2249173a76fc781
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-14 22:07:35 +0530

    Updates

--- PS-Sheet2/Problem36.java
+++ PS-Sheet2/Problem36.java
@@ -11,6 +11,16 @@ Explanation: The first 4 even numbers are 2, 4, 6, 8, and their sum is 20
  */
 public class Problem36 {
     public static void main(String[] args) {
-        
+        int N = 4;
+        int sum = sumOfFirstNEvenNumbers(N);
+        System.out.println(sum);
+    }
+
+    public static int sumOfFirstNEvenNumbers(int N) {
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += 2 * i;
+        }
+        return sum;
     }
 }
