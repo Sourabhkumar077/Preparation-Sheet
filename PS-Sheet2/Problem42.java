e81d07c28165068bffa69f0a757643b644c504b5
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-17 12:33:15 +0530

    New solution

--- PS-Sheet2/Problem42.java
+++ PS-Sheet2/Problem42.java
@@ -10,6 +10,24 @@ Explanation: The largest elements in each row are 3, 6, and 9.
  */
 public class Problem42 {
     public static void main(String[] args) {
-        
+        int mat[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[] result = largestElementInEachRow(mat);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+    }
+    public static int[] largestElementInEachRow(int[][] matrix) {
+        int[] result = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            int max = matrix[i][0];
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+            result[i] = max;
+        }
+        return result;
     }
 }
