1235d363b4491aba61f03bb2debb06ec79bd2f42
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-17 12:31:04 +0530

    New solution

--- PS-Sheet2/Problem45.java
+++ PS-Sheet2/Problem45.java
@@ -10,8 +10,32 @@ Output:
 3 8 1  
 7 4 6 
  */
+
+import java.util.Scanner;
+
 public class Problem45 {
+    public static void generateRandomNumMatrix(int rows, int columns) {
+        int[][] matrix = new int[rows][columns];
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                matrix[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");        
+        int columns = sc.nextInt();
+        generateRandomNumMatrix(rows, columns);
     }
 }
