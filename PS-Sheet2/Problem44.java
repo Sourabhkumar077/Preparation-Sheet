c899c6f35a73f3ffe55056ee09edee48c93ada72
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-17 12:41:35 +0530

    solution added

--- PS-Sheet2/Problem44.java
+++ PS-Sheet2/Problem44.java
@@ -8,9 +8,32 @@ Input: strings = ["listen", "silent", "hello", "world"]
 Output: [("listen", "silent")]
 Explanation: "listen" and "silent" are anagrams.
  */
+
+import java.util.Arrays;
+
 public class Problem44 {
 
     public static void main(String[] args) {
-        
+        String[] strings = { "listen", "silent", "hello", "world" };
+        findAnagramPairs(strings);
     }
+
+    public static void findAnagramPairs(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (isAnagram(strings[i], strings[j])) {
+                    System.out.println("(" + strings[i] + ", " + strings[j] + ")");
+                }
+            }
+        }
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        return Arrays.equals(chars1, chars2);
+    }
+
 }
