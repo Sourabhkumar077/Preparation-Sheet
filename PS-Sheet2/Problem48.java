00b03cea49cbc76c0d07e1f55453e1255d880ee2
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-18 19:00:35 +0530

    updates

--- PS-Sheet2/Problem48.java
+++ PS-Sheet2/Problem48.java
@@ -11,8 +11,23 @@ Output:
 2 6 9  
 1 8 4
  */
+
+import java.util.Scanner;
+
 public class Problem48 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the matrix: ");
+        int size = sc.nextInt();
+        sc.close();
+        generateMatrixRandom(size);
+    }
+    public static void generateMatrixRandom(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print((int) (Math.random() * 10) + " ");
+            }
+            System.out.println();
+        }
     }
 }
