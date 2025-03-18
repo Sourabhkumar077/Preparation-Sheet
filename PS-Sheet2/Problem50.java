7f53ee8812cb01d2e35b579bb9fd902d929c886f
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-18 18:56:35 +0530

    Updates

--- PS-Sheet2/Problem50.java
+++ PS-Sheet2/Problem50.java
@@ -12,8 +12,23 @@ Output:
 ***  
 ****  
  */
+
+import java.util.Scanner;
+
 public class Problem50 {
     public static void main(String[] args) {
-        
+    Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height of the triangle: ");
+        int n = sc.nextInt();  
+        sc.close();
+        generateTriangle(n);
+    }
+    public static void generateTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
