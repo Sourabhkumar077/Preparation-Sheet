bc225834829fa894397e1ccd3ac8432951130a7f
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-20 21:50:54 +0530

    Updates

--- PS-Sheet3/Problem01.java
+++ PS-Sheet3/Problem01.java
@@ -11,9 +11,20 @@ Output:
 ***
 ****
  */
+
 public class Problem01 {
 
     public static void main(String[] args) {
+       int rows = 4;
+        printPattern(rows);
         
+    }
+    public static void printPattern(int rows) { 
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
