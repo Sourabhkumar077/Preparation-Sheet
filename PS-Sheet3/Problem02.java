b562067732c3f0bfbaaf2a62a1a55bcd32c97592
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-20 21:53:29 +0530

    New solution added

--- PS-Sheet3/Problem02.java
+++ PS-Sheet3/Problem02.java
@@ -16,6 +16,15 @@ Output:
  */
 public class Problem02 {
     public static void main(String[] args) {
-        
+        int n = 5;
+        printPattern(n);// Call the function with n = 5
+    }
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
--- PS-Sheet3/Problem03.java
+++ PS-Sheet3/Problem03.java
@@ -14,6 +14,18 @@ Output:
  */
 public class Problem03 {
     public static void main(String[] args) {
-        
+       int n = 3;
+        printPattern(n);// Call the function with n = 3  
+    }
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
