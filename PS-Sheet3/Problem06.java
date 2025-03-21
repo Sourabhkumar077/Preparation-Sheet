54d1a3aa68062b4970b205f0f1d805b6814ddcfa
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-21 20:37:53 +0530

    new solution added

--- PS-Sheet3/Problem06.java
+++ PS-Sheet3/Problem06.java
@@ -14,6 +14,15 @@ Output:
  */
 public class Problem06 {
     public static void main(String[] args) {
-        
+        int n = 4;
+        printPattern(n);
+    }
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
