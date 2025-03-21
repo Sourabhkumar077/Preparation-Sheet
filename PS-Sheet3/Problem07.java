1f68e9d997dafdb30b38608f472ff97b5580c018
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-21 20:43:43 +0530

    new solution added

--- PS-Sheet3/Problem07.java
+++ PS-Sheet3/Problem07.java
@@ -15,6 +15,18 @@ Output:
  */
 public class Problem07 {
     public static void main(String[] args) {
-        
+        int n = 5;
+        printPattern(n);
     }
+    public static void printPattern(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" "); // print spaces
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" *"); // print asterisks
+            }
+            System.out.println();
+        } 
+     }
 }
