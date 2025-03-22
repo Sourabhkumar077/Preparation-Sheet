f569d016a1d77281b3b469173008878e9ba76445
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-23 00:10:10 +0530

    new chamges

--- PS-Sheet3/Problem08.java
+++ PS-Sheet3/Problem08.java
@@ -13,8 +13,24 @@ Output:
  121
   1
  */
+/*
+ * Problem 8: Print a Diamond Pattern with Numbers
+Difficulty: Easy
+Topics: Pattern Printing
+Hint: Print a diamond pattern with numbers. The pattern should have a peak in the middle with symmetric rows above and below it.
+
+Example 1: Input: n = 3
+Output:
+
+  1
+ 121
+12321
+ 121
+  1
+ */
 public class Problem08 {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+      
+  }
 }
+
--- PS-Sheet3/Problem09.java
+++ PS-Sheet3/Problem09.java
@@ -14,6 +14,17 @@ Output:
  */
 public class Problem09 {
     public static void main(String[] args) {
-        
+        int n = 4;
+        printPattern(n);
+    }
+    public static void printPattern(int n) {
+       int num = 1;
+       for (int i = 1; i <= n; i++) {
+           for (int j = 1; j <= i; j++) {
+               System.out.print(num + " "); // print numbers
+               num++;
+           }
+           System.out.println();
+       } 
     }
 }
