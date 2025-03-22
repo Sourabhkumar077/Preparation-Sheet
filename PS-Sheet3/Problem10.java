b86cad9fba913423ef80cf0d3d70b1d5c3890af4
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-23 00:03:49 +0530

    new solution

--- PS-Sheet3/Problem10.java
+++ PS-Sheet3/Problem10.java
@@ -12,7 +12,30 @@ Output:
 34543
  */
 public class Problem10 {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+      int n = 3; 
+      printPattern(n); 
+  }
+
+  public static void printPattern(int n) {
+      for (int i = 1; i <= n; i++) {
+          // Print leading spaces
+          for (int j = 1; j <= n - i; j++) {
+              System.out.print(" ");
+          }
+
+          // Print increasing numbers
+          for (int k = 1; k <= i; k++) {
+              System.out.print(i + k - 1);
+          }
+
+          // Print decreasing numbers
+          for (int k = i - 1; k >= 1; k--) {
+              System.out.print(i + k - 1);
+          }
+
+          // Move to the next line
+          System.out.println();
+      }
+  }
 }
