92b1ae21d683edbd003278c838f5d9fe24ea1aff
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-25 23:03:09 +0530

    updates

--- PS-Sheet3/Problem08.java
+++ PS-Sheet3/Problem08.java
@@ -30,11 +30,45 @@ Output:
  */
 public class Problem08 {
   public static void main(String[] args) {
-      int size= 5;
+      int size = 3;
       printPattern(size);
   }
-  public static void printPattern(int size){
 
+  public static void printPattern(int size) {
+      // Upper half of the pattern
+      for (int i = 1; i <= size; i++) {
+          // Print leading spaces
+          for (int j = 1; j <= size - i; j++) {
+              System.out.print(" ");
+          }
+          // Print increasing numbers
+          for (int j = 1; j <= i; j++) {
+              System.out.print(j);
+          }
+          // Print decreasing numbers
+          for (int j = i - 1; j >= 1; j--) {
+              System.out.print(j);
+          }
+          System.out.println();
+      }
+
+      // Lower half of the pattern
+      for (int i = size - 1; i >= 1; i--) {
+          // Print leading spaces
+          for (int j = 1; j <= size - i; j++) {
+              System.out.print(" ");
+          }
+          // Print increasing numbers
+          for (int j = 1; j <= i; j++) {
+              System.out.print(j);
+          }
+          // Print decreasing numbers
+          for (int j = i - 1; j >= 1; j--) {
+              System.out.print(j);
+          }
+          System.out.println();
+      }
   }
 }
+
 
