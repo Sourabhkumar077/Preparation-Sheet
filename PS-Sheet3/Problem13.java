33059203b2ec0104472b7dc4ec4f3841bafdac6d
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-27 19:08:15 +0530

    updates

--- PS-Sheet3/Problem13.java
+++ PS-Sheet3/Problem13.java
@@ -14,6 +14,24 @@ Output:
  */
 public class Problem13 {
     public static void main(String[] args) {
-        
+        int n = 3;
+        printPattern(n);
+    }
+/*************  ✨ Codeium Command ⭐  *************/
+    /**
+     * Prints a matrix of size n x n with consecutive numbers starting from 1,
+     * filling rows sequentially.
+     *
+     * @param n the size of the matrix
+     */
+/******  05c8c6cc-fdf1-4e67-b796-939444205cb9  *******/
+    public static void printPattern(int n) {
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(count++ + " ");
+            }
+            System.out.println();
+        }
     }
 }
