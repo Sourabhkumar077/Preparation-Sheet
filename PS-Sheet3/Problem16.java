9c8a577d15522fab3ed715551e83db6c04b19f19
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-27 22:11:20 +0530

    new solution

--- PS-Sheet3/Problem16.java
+++ PS-Sheet3/Problem16.java
@@ -14,6 +14,19 @@ OXOXOX
  */
 public class Problem16 {
     public static void main(String[] args) {
-        
+     int n = 4;
+     printPattern(n);   
     }
+    public static void printPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O " );
+                }
+            }
+            System.out.println();
+        }
+    }   
 }
--- PS-Sheet3/Problem17.java
+++ PS-Sheet3/Problem17.java
@@ -13,6 +13,18 @@ Output:
  */
 public class Problem17 {
     public static void main(String[] args) {
-        
+       int n = 3;
+       printPattern(n); 
     }
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }   
 }
