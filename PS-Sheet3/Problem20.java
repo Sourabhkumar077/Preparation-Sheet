12e224ad804df3220af85b67c67153ce633e049a
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-28 23:49:24 +0530

    updates

--- PS-Sheet3/Problem20.java
+++ PS-Sheet3/Problem20.java
@@ -14,7 +14,28 @@ Output:
 ***
  */
 public class Problem20 {
-    public static void main(String[] args) {
-        
+  public static void main(String[] args) {
+    int n = 5; // input value
+    printCrossPattern(n);
+  }
+
+  public static void printCrossPattern(int n) {
+    // print top and bottom rows with stars
+    for (int i = 0; i < n; i++) {
+      if (i == 0 || i == n - 1) {
+        for (int j = 0; j < n; j++) {
+          System.out.print("*");
+        }
+      } else {
+        for (int j = 0; j < n; j++) {
+          if (j == 0 || j == n - 1 || i == j) {
+            System.out.print("*");
+          } else {
+            System.out.print(" ");
+          }
+        }
+      }
+      System.out.println();
     }
+  }
 }
