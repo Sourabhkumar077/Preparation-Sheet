ccc121ace61b546099a57a643b9907022957d133
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-28 23:52:25 +0530

    updates

--- PS-Sheet3/Problem18.java
+++ PS-Sheet3/Problem18.java
@@ -14,6 +14,31 @@ Output:
  */
 public class Problem18 {
     public static void main(String[] args) {
-        
+      int n = 4; // input value
+      printBorderPattern(n);
     }
-}
+  
+    public static void printBorderPattern(int n) {
+      // print top row with numbers
+      for (int i = 1; i <= n; i++) {
+        System.out.print(i);
+      }
+      System.out.println();
+  
+      // print middle rows with border numbers
+      for (int i = 1; i <= n - 2; i++) {
+        System.out.print(1);
+        for (int j = 1; j <= n - 2; j++) {
+          System.out.print(" ");
+        }
+        System.out.print(1);
+        System.out.println();
+      }
+  
+      // print bottom row with numbers
+      for (int i = 1; i <= n; i++) {
+        System.out.print(i);
+      }
+      System.out.println();
+    }
+  }
