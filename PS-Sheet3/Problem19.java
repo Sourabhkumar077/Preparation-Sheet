6bb89ca061e57304b987cc718e4950c24c7284cc
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-28 10:31:47 +0530

    new solution

--- PS-Sheet3/Problem19.java
+++ PS-Sheet3/Problem19.java
@@ -13,6 +13,18 @@ CCC
  */
 public class Problem19 {
     public static void main(String[] args) {
-        
+       int n = 3;
+       printPattern(n); 
+    }
+    public static void printPattern(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int= j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
