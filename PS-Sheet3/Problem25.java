884883ae4272eacbd7b2191ff223c0b372406cac
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-31 23:23:02 +0530

    updates

--- PS-Sheet3/Problem25.java
+++ PS-Sheet3/Problem25.java
@@ -14,6 +14,32 @@ Output:
  */
 public class Problem25 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printZigzagMatrix(n);
+    }
+
+    public static void printZigzagMatrix(int n) {
+        int[][] matrix = new int[n][n * 2];
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) { // even row, print from left to right
+                for (int j = 0; j < n * 2; j++) {
+                    matrix[i][j] = num++;
+                }
+            } else { // odd row, print from right to left
+                for (int j = n * 2 - 1; j >= 0; j--) {
+                    matrix[i][j] = num++;
+                }
+            }
+        }
+
+        // print the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 2; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
