c99cfe383d685fa14301174638b6ace9e19193b0
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-03-31 23:24:31 +0530

    changes

--- PS-Sheet3/Problem24.java
+++ PS-Sheet3/Problem24.java
@@ -15,6 +15,35 @@ Output:
  */
 public class Problem24 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printStarPattern(n);
+    }
+
+    public static void printStarPattern(int n) {
+        // print increasing stars
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // print decreasing stars
+        for (int i = n - 1; i >= 1; i--) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
