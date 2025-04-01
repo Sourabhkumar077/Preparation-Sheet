eec4608836ab2be02d59172d1c4db0a2f7c83c6c
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-01 22:39:24 +0530

    updates

--- PS-Sheet3/Problem22.java
+++ PS-Sheet3/Problem22.java
@@ -14,7 +14,36 @@ Output:
   *
  */
 public class Problem22 {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+      int n = 3; // input value
+      printDiamondPattern(n);
+  }
+
+  public static void printDiamondPattern(int n) {
+      // print top half of the diamond
+      for (int i = 1; i <= n; i++) {
+          // print spaces
+          for (int j = 1; j <= n - i; j++) {
+              System.out.print(" ");
+          }
+          // print stars
+          for (int j = 1; j <= i * 2 - 1; j++) {
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+
+      // print bottom half of the diamond
+      for (int i = n - 1; i >= 1; i--) {
+          // print spaces
+          for (int j = 1; j <= n - i; j++) {
+              System.out.print(" ");
+          }
+          // print stars
+          for (int j = 1; j <= i * 2 - 1; j++) {
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+  }
 }
