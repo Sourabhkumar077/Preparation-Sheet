6ff1b37d4dd0302d6c094b4d325f51f7a81cf5be
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-01 22:40:56 +0530

    updates

--- PS-Sheet3/Problem23.java
+++ PS-Sheet3/Problem23.java
@@ -14,7 +14,44 @@ Output:
   1
  */
 public class Problem23 {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+      int n = 3; // input value
+      printDiamondPattern(n);
+  }
+
+  public static void printDiamondPattern(int n) {
+      // print top half of the diamond
+      for (int i = 1; i <= n; i++) {
+          // print spaces
+          for (int j = 1; j <= n - i; j++) {
+              System.out.print(" ");
+          }
+          // print numbers
+          for (int j = 1; j <= i; j++) {
+              System.out.print(j);
+          }
+          // print numbers in reverse order
+          for (int j = i - 1; j >= 1; j--) {
+              System.out.print(j);
+          }
+          System.out.println();
+      }
+
+      // print bottom half of the diamond
+      for (int i = n - 1; i >= 1; i--) {
+          // print spaces
+          for (int j = 1; j <= n - i; j++) {
+              System.out.print(" ");
+          }
+          // print numbers
+          for (int j = 1; j <= i; j++) {
+              System.out.print(j);
+          }
+          // print numbers in reverse order
+          for (int j = i - 1; j >= 1; j--) {
+              System.out.print(j);
+          }
+          System.out.println();
+      }
+  }
 }
