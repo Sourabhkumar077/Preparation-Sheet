f27f6ff1ed144555f87ad7d5b7232cdcc8462d50
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-02 19:47:52 +0530

    updates

--- PS-Sheet3/Problem30.java
+++ PS-Sheet3/Problem30.java
@@ -19,6 +19,35 @@ Output:
  */
 public class Problem30 {
     public static void main(String[] args) {
-        
+        int n = 5; // input value
+        printCrossPattern(n);
+    }
+
+    public static void printCrossPattern(int n) {
+        // print top half of the cross
+        for (int i = 0; i < n; i++) {
+            // print spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // print bottom half of the cross
+        for (int i = n - 2; i >= 0; i--) {
+            // print spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
