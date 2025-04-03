290fdaea3ef3454af823fc6603d81429573f2a3c
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-03 22:51:46 +0530

    updates

--- PS-Sheet3/Problem27.java
+++ PS-Sheet3/Problem27.java
@@ -12,7 +12,24 @@ Output:
 EFGHI
  */
 public class Problem27 {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+      int n = 3; // input value
+      printPyramidPattern(n);
+  }
+
+  public static void printPyramidPattern(int n) {
+      char c = 'A'; // starting character
+      for (int i = 1; i <= n; i++) {
+          // print spaces
+          for (int j = 1; j <= n - i; j++) {
+              System.out.print(" ");
+          }
+          // print characters
+          for (int j = 1; j <= i * 2 - 1; j++) {
+              System.out.print(c);
+              c++; // increment character
+          }
+          System.out.println();
+      }
+  }
 }
