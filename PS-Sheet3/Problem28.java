675a0d15db0b765a319d2c2dbffebbc496580a7b
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-03 22:50:41 +0530

    updates

--- PS-Sheet3/Problem28.java
+++ PS-Sheet3/Problem28.java
@@ -14,6 +14,25 @@ D     D
  */
 public class Problem28 {
     public static void main(String[] args) {
-        
+        int n = 4; // input value
+        printDiagonalPattern(n);
+    }
+
+    public static void printDiagonalPattern(int n) {
+        char c = 'A'; // starting character
+        for (int i = 1; i <= n; i++) {
+            // print first character
+            System.out.print(c);
+            // print spaces
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(" ");
+            }
+            // print second character
+            if (i > 1) {
+                System.out.print(c);
+            }
+            System.out.println();
+            c++; // increment character
+        }
     }
 }
