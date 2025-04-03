1ffe853ac7a61115d5cfd3642d1016113bf9f844
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-03 22:52:46 +0530

    new solution

--- PS-Sheet3/Problem26.java
+++ PS-Sheet3/Problem26.java
@@ -14,6 +14,23 @@ BABA
  */
 public class Problem26 {
     public static void main(String[] args) {
-        
+        int n = 4; // input value
+        printAlternatingPattern(n);
+    }
+
+    public static void printAlternatingPattern(int n) {
+        char c1 = 'A'; // first character
+        char c2 = 'B'; // second character
+        for (int i = 1; i <= n; i++) {
+            // print row
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(c2);
+                } else {
+                    System.out.print(c1);
+                }
+            }
+            System.out.println();
+        }
     }
 }
