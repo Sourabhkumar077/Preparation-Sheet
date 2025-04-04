813216f5727c27fab7010eb7de2e37b56da6eb27
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-04 23:57:30 +0530

    updates

--- PS-Sheet3/Problem31.java
+++ PS-Sheet3/Problem31.java
@@ -1,5 +1,15 @@
 /*
- * 
+ * roblem 31: Print a Triangular Matrix with Numbers
+Difficulty: Easy
+Topics: Pattern Printing
+Hint: Print a triangular matrix where each row contains increasing numbers. Each subsequent row should start from the next number.
+
+Example 1: Input: n = 3
+Output:
+
+1
+2 3
+4 5 6
  */
 public class Problem31 {
     public static void main(String[] args) {
--- PS-Sheet3/Problem32.java
+++ PS-Sheet3/Problem32.java
@@ -1,3 +1,21 @@
+/*
+ * Problem 32: Print a Star Pattern with Increasing and Decreasing Width
+Difficulty: Medium
+Topics: Pattern Printing
+Hint: Print a pattern with stars that increase to a midpoint and then decrease. The stars should be centered horizontally.
+
+Example 1: Input: n = 3
+Output:
+
+  *
+ ***
+*****
+ ***
+  *
+
+ */
 public class Problem32 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
