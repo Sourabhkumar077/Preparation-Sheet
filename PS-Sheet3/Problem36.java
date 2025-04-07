ea06763cdbe51e1f04b684c2b0776f220d1e46ce
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-08 00:36:15 +0530

    new files added

---
+++ PS-Sheet3/Problem36.java
@@ -0,0 +1,5 @@
+public class Problem36 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ PS-Sheet3/Problem38.java
@@ -0,0 +1,5 @@
+public class Problem38 {
+    public static void main(String[] args) {
+        
+    }
+}
