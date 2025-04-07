f5894b327f6cfbfc0980ac7c89a79b7a38572357
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-07 22:17:00 +0530

    updates

--- PS-Sheet3/Problem34.java
+++ PS-Sheet3/Problem34.java
@@ -13,6 +13,19 @@ D E F
  */
 public class Problem34 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printIncreasingColumns(n);
+    }
+
+    public static void printIncreasingColumns(int n) {
+        char c = 'A'; // starting character
+        for (int i = 1; i <= n; i++) {
+            // print row
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c + " ");
+                c++; // increment character
+            }
+            System.out.println();
+        }
     }
 }
