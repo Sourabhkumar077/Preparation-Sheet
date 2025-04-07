d799bc156d414474436de5f486de899e4c8394e7
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-07 22:16:25 +0530

    new solution

--- PS-Sheet3/Problem35.java
+++ PS-Sheet3/Problem35.java
@@ -13,6 +13,51 @@ Output:
  */
 public class Problem35 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printSpiralMatrix(n);
+    }
+
+    public static void printSpiralMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+        while (num <= n * n) {
+            // fill top row from left to right
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            // fill right column from top to bottom
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            // fill bottom row from right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // fill left column from bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        // print the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
