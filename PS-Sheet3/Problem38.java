db9deb2067099d28c8d61fc68fe56d5fb5519de0
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-09 20:47:14 +0530

    updates

--- PS-Sheet3/Problem38.java
+++ PS-Sheet3/Problem38.java
@@ -15,6 +15,35 @@ Output:
  */
 public class Problem38 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printAlternatingTriangles(n);
+    }
+
+    public static void printAlternatingTriangles(int n) {
+        // print top half of the pattern
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // print bottom half of the pattern
+        for (int i = n - 1; i >= 1; i--) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
