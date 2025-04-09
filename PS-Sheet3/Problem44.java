20c03259a6fde27db2772054949774f71f6c7989
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-09 20:59:18 +0530

    updates

--- PS-Sheet3/Problem44.java
+++ PS-Sheet3/Problem44.java
@@ -13,6 +13,18 @@ Output:
  */
 public class Problem44 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printMatrix(n);
+    }
+
+    public static void printMatrix(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
     }
 }
--- PS-Sheet3/Problem45.java
+++ PS-Sheet3/Problem45.java
@@ -13,6 +13,18 @@ DEF
  */
 public class Problem45 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printPattern(n);
+    }
+
+    public static void printPattern(int n) {
+        char c = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c);
+                c++;
+            }
+            System.out.println();
+        }
     }
 }
