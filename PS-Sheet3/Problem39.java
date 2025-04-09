7babeae63fa2ad130866cdcc4b247ae81f2eae5a
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-09 20:50:15 +0530

    updates

--- PS-Sheet3/Problem39.java
+++ PS-Sheet3/Problem39.java
@@ -12,9 +12,43 @@ Output:
 12321
  121
   1
- */
-public class Problem39 {
+ */public class Problem39 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printDiamondMatrix(n);
+    }
+
+    public static void printDiamondMatrix(int n) {
+        // print top half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // print numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + j - 1 + " ");
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(i + j - 2 + " ");
+            }
+            System.out.println();
+        }
+
+        // print bottom half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // print numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + j - 1 + " ");
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(i + j - 2 + " ");
+            }
+            System.out.println();
+        }
     }
 }
