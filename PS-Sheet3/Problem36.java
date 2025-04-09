6ad40eba89613e0872d9fc0c48c1f8574d75b93f
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-09 20:42:57 +0530

    updates

--- PS-Sheet3/Problem36.java
+++ PS-Sheet3/Problem36.java
@@ -13,6 +13,20 @@ XOX
  */
 public class Problem36 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printCheckerboardPattern(n);
+    }
+
+    public static void printCheckerboardPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(" X ");
+                } else {
+                    System.out.print(" O ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
