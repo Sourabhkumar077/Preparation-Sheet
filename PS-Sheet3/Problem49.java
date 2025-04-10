b7d1b1fb58a0084a0fcb508b7ed8fa58101b7285
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-10 23:29:31 +0530

    updates

--- PS-Sheet3/Problem49.java
+++ PS-Sheet3/Problem49.java
@@ -14,6 +14,18 @@ Output:
  */
 public class Problem49 {
     public static void main(String[] args) {
-        
+        int n = 3; // input value
+        printPattern(n);
+    }
+
+    public static void printPattern(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
     }
 }
