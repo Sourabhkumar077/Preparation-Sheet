72708a394fc0fb44e946a6f45ddef3d9f8e3ad56
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-04-11 22:32:23 +0530

    updates

--- PS-Sheet3/Problem48.java
+++ PS-Sheet3/Problem48.java
@@ -15,6 +15,21 @@ Output:
  */
 public class Problem48 {
     public static void main(String[] args) {
-        
+        int n = 5; // You can change this value to alter the size of the pattern
+        printConcentricSquares(n);
+    }
+
+    public static void printConcentricSquares(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Approach: Print '*' at the borders (first and last rows/columns) to form a hollow square; inside the borders, print spaces.
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // Move to the next row
+        }
     }
 }
